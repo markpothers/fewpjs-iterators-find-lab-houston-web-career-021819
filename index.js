f21bc5d44bc7510181df38e5d3a56ec49570d877
Mark R Pothecary <markpothers@hotmail.com>
2019-04-11 13:18:46 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,34 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+// function superbowlWin(array){
+//   let result = array.find( (item) => item["result"] === "W" )
+//   if (result != undefined){
+//     return result["year"]
+//   }else{
+//     return result
+//   }
+// }
+
+// function superbowlWin(array){
+//   if ((array.find( (item) => item["result"] === "W" ) != undefined)){
+//     return (array.find( (item) => item["result"] === "W" ))["year"]
+//   }else{
+//     return array.find( (item) => item["result"] === "W" )
+//   }
+// }
+
+// function superbowlWin(array){
+//   const finder = (array.find( (item) => item["result"] === "W" ))
+//   if (finder != undefined){
+//     return finder["year"]
+//   }else{
+//     return finder
+//   }
+// }
+
+function superbowlWin(array){
+  const finder = (array.find( item => item["result"] === "W" ))
+  return (finder) ? finder.year : finder
+}
